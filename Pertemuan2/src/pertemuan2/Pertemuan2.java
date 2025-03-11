2687eab4cbf9cc1228929b2d07b43452fd7d8f7d
Theresia2029 <ichamude@gmail.com>
2025-03-11 21:56:15 +0800

    pertemuan 2

---
+++ Pertemuan2/src/pertemuan2/Pertemuan2.java
@@ -0,0 +1,26 @@
+
+ 
+package pertemuan2;
+
+/**
+ *
+ * @author Maris Theresia Mude
+ * tgl 11 Maret 2025
+ */ 
+public class Pertemuan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama = "Maria Theresia Mude";
+        String nim ="21103156";
+        System.out.print("Pertemuan 2 java");
+        System.out.print(";;;;;;;;;;;;;;;;");
+        System.out.print("Nama saya : "+nama);
+        System.out.print("NIM :"+nim);
+        System.out.printf("nama saya %S,saat ini sedang belajar pemrograman bahasa java",nama,nim);
+        
+    }
+    
+}
